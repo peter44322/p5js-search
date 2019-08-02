bbb8dc9b911f78eca2d8ad29704b3defe93672de
peter44322 <p.pator@outlook.com>
2019-08-02 08:55:24 +0200

    first commit

--- app.pl
+++ app.pl
@@ -1,23 +1,23 @@
-test :-
-    go(s(['.', '.', "#", '.', "#", '.', "*", '.', '.', '.', "#", '.'], 2, [1, 2, 3, 4], 1),
-       s(_, 0, _, _)).
+test(R) :-
+    go(s([0, 0, 1, 0, 1, 0, 2, 0, 0, 0, 1, 0], 10, [1, 2, 3, 4], 0), R).
 
-:- use_module(library(lists)).
-move(S, Snew) :-
+
+    :- use_module(library(lists)).
+    move(S, Snew) :-
     up(S, Snew).
-move(S, Snew) :-
+    move(S, Snew) :-
     right(S, Snew).
-move(S, Snew) :-
+    move(S, Snew) :-
     left(S, Snew).
-move(S, Snew) :-
+    move(S, Snew) :-
     down(S, Snew).
 
-up(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
+    up(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
     X>=Ucost,
-    not(R00="*"),
-    not(R01="*"),
-    not(R02="*"),
-    not(R03="*"),
+    R00\=2,
+    R01\=2,
+    R02\=2,
+    R03\=2,
     nth0(I,
          
          [ R00,
@@ -33,7 +33,7 @@ up(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rc
            R22,
            R23
          ],
-         "*"),
+         2),
     AboveI is I-4,
     nth0(AboveI,
          
@@ -51,8 +51,8 @@ up(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rc
            R23
          ],
          Above),
-    not(Above="#"),
-    substitute("*",
+    Above\=1,
+    substitute(2,
                
                [ R00,
                  R01,
@@ -67,20 +67,20 @@ up(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rc
                  R22,
                  R23
                ],
-               ".",
+               0,
                NewList),
-    replace(AboveI, NewList, "*", FinalList),
+    replace(AboveI, NewList, 2, FinalList),
     NewX is X-Ucost,
     NewNum is Num+1.
-  
 
 
-down(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
+
+    down(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
     X>=Dcost,
-    not(R20="*"),
-    not(R21="*"),
-    not(R22="*"),
-    not(R23="*"),
+    R20\=2,
+    R21\=2,
+    R22\=2,
+    R23\=2,
     nth0(I,
          
          [ R00,
@@ -96,7 +96,7 @@ down(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
            R22,
            R23
          ],
-         "*"),
+         2),
     BelowI is I+4,
     nth0(BelowI,
          
@@ -114,8 +114,8 @@ down(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
            R23
          ],
          Below),
-    not(Below="#"),
-    substitute("*",
+    Below\=1,
+    substitute(2,
                
                [ R00,
                  R01,
@@ -130,17 +130,17 @@ down(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
                  R22,
                  R23
                ],
-               ".",
+               0,
                NewList),
-    replace(BelowI, NewList, "*", FinalList),
+    replace(BelowI, NewList, 2, FinalList),
     NewX is X-Dcost,
     NewNum is Num+1.
 
-left(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
+    left(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
     X>=Lcost,
-    not(R00="*"),
-    not(R10="*"),
-    not(R20="*"),
+    R00\=2,
+    R10\=2,
+    R20\=2,
     nth0(I,
          
          [ R00,
@@ -156,7 +156,7 @@ left(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
            R22,
            R23
          ],
-         "*"),
+         2),
     LeftI is I-1,
     nth0(LeftI,
          
@@ -174,8 +174,8 @@ left(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
            R23
          ],
          Left),
-    not(Left="#"),
-    substitute("*",
+    Left\=1,
+    substitute(2,
                
                [ R00,
                  R01,
@@ -190,17 +190,17 @@ left(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, 
                  R22,
                  R23
                ],
-               ".",
+               0,
                NewList),
-    replace(LeftI, NewList, "*", FinalList),
+    replace(LeftI, NewList, 2, FinalList),
     NewX is X-Lcost,
     NewNum is Num+1.
 
-right(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
+    right(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost, Rcost, Ucost, Dcost], Num), s(FinalList, NewX, [Lcost, Rcost, Ucost, Dcost], NewNum)) :-
     X>=Rcost,
-    not(R03="*"),
-    not(R13="*"),
-    not(R23="*"),
+    R03\=2,
+    R13\=2,
+    R23\=2,
     nth0(I,
          
          [ R00,
@@ -216,7 +216,7 @@ right(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost,
            R22,
            R23
          ],
-         "*"),
+         2),
     RightI is I+1,
     nth0(RightI,
          
@@ -234,8 +234,8 @@ right(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost,
            R23
          ],
          Right),
-    not(Right="#"),
-    substitute("*",
+    Right\=1,
+    substitute(2,
                
                [ R00,
                  R01,
@@ -250,74 +250,91 @@ right(s([R00, R01, R02, R03, R10, R11, R12, R13, R20, R21, R22, R23], X, [Lcost,
                  R22,
                  R23
                ],
-               ".",
+               0,
                NewList),
-    replace(RightI, NewList, "*", FinalList),
+    replace(RightI, NewList, 2, FinalList),
     NewX is X-Rcost,
     NewNum is Num+1.
 
 
 
-replace(I, L, E, K) :-
+    replace(I, L, E, K) :-
     nth0(I, L, _, R),
     nth0(I, K, E, R).
-  
-substitute(_, [], _, []) :-
+
+    substitute(_, [], _, []) :-
     !.
-substitute(X, [X|T], Y, [Y|T1]) :-
+    substitute(X, [X|T], Y, [Y|T1]) :-
     substitute(X, T, Y, T1),
     !.
 
-substitute(X, [H|T], Y, [H|T1]) :-
+    substitute(X, [H|T], Y, [H|T1]) :-
     substitute(X, T, Y, T1).
 
-  %query of user and takes start state and next state
-go(Start, Goal) :-
-    path([[Start, null]], [], Goal).
+      %query of user and takes start state and next state
+go(Start, R) :-
+    bagof(X, path([[Start, null]], [], X), Children),
+    distinct(Children, X),
+    length(X, M),
+    R is M-1.
 
-%main predicate that takes open list, closed list and goal state
 path([], _, _) :-
-    write('No solution'),
-    nl,
     !.
+
 path([[Goal, Parent]|_], Closed, Goal) :-
-    write('Goal solution is found'),
     nl,
-    printsolution([Goal, Parent], Closed),
-    !.
+    printsolution([Goal, Parent], Closed).
+
+    %main predicate that takes open list, closed list and goal state
 path(Open, Closed, Goal) :-
     removeFromOpen(Open, [State, Parent], RestOfOpen),
     getchildren(State, Open, Closed, Children),
     addListToOpen(Children, RestOfOpen, NewOpen),
     path(NewOpen, [[State, Parent]|Closed], Goal).
 
-%gets Children of State that aren't in Open or Close
+    %gets Children of State that aren't in Open or Close
 getchildren(State, Open, Closed, Children) :-
     bagof(X, moves(State, Open, Closed, X), Children),
     !.
-getchildren(_, _, _, []).
+    getchildren(_, _, _, []).
 
-%adds children to open list (without head child) to form new open list
-%here it is like append i.e.Breadth First
+    %adds children to open list (without head child) to form new open list
+    %here it is like append i.e.Breadth First
 addListToOpen(Children, [], Children).
-addListToOpen(Children, [H|Open], [H|NewOpen]) :-
+    addListToOpen(Children, [H|Open], [H|NewOpen]) :-
     addListToOpen(Children, Open, NewOpen).
 
-%gets head of open list to get its children later
+    %gets head of open list to get its children later
 removeFromOpen([State|RestOpen], State, RestOpen).
 
-%gets next state given the current state
+    %gets next state given the current state
 moves(State, Open, Closed, [Next, State]) :-
     move(State, Next),
     \+ member([Next, _], Open),
     \+ member([Next, _], Closed).
 
-%prints the path from start state to goal state
+    %prints the path from start state to goal state
 printsolution([State, null], _) :-
     write(State),
     nl.
-printsolution([State, Parent], Closed) :-
+    printsolution([State, Parent], Closed) :-
     member([Parent, GrandParent], Closed),
     printsolution([Parent, GrandParent], Closed),
     write(State),
     nl.
+
+
+
+    member1(s(X, _, _, _), [s(H, _, _, _)|_]) :-
+    X==H,
+    !.
+    member1(X, [_|T]) :-
+    member1(X, T).
+
+    distinct([], []).
+    distinct([H|T], C) :-
+    member1(H, T),
+    !,
+    distinct(T, C).
+    distinct([H|T], [H|C]) :-
+    distinct(T, C).
